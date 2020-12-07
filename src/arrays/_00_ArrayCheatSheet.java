ef0e40e4da78dda6540dda069ab98d52c5cbc77d
root <root@a6f69a570524>
2020-12-07 21:30:38 +0000

    robot race figuring out circle track

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,6 +1,9 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
+	static int small = 0;
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
 		String array[] = {"Taco", "Quakko", "Mako", "Lokko", "Walko"};
@@ -13,19 +16,39 @@ public class _00_ArrayCheatSheet {
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		for(int i = 0; i < 5; i++) {
-			int alv = 0;
-			System.out.println(array[alv]);
-			alv++;
+			System.out.println(array[i]);
 		}
 		
 		//6. make an array of 50 integers
-		int [] a = new int {50};
+		int [] a = new int [50];
 		//7. use a for loop to make every value of the integer array a random number
+		for(int is = 0; is < 5; is++) {
+		Random r = new Random();
+		int ran = r.nextInt(50);
+			 a [is]= ran;
+		}
+		small = a [0];
+		for(int iss = 0; iss < 50; iss++) {
+			if(small > a [iss]) {
+				small = a [iss];
+			}
+			
+		}
+		System.out.println(small);
 		
 		//8. without printing the entire array, print only the smallest number in the array
 		
 		//9 print the entire array to see if step 8 was correct
-		
+		for(int j = 0; j < 50; j++) {
+			System.out.println(a [j]);
+		}
+		small = a [0];
 		//10. print the largest number in the array.
+		for(int isss = 0; isss < 50; isss++) {	
+			if(small < a [isss]) {
+				small = a [isss];
+			}
+	}
+		System.out.println(small);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,8 +1,49 @@
 package arrays;
 
-public class _01_RobotRace {
-	//1. make a main method
+import java.util.Random;
 
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class _01_RobotRace {
+	static //1. make a main method
+	int winnerSaver;
+public static void main(String[] args) {
+Robot [] robot = new Robot[5];
+
+for(int i = 0; i < 5; i++) {
+	int difference = i*100;
+robot [i] = new Robot();
+robot [i].setX(50 + difference);
+robot [i].setY(500);
+}
+boolean raceOver = false;
+while(raceOver == false) {
+for(int is = 0; is < 5; is++) {
+	Random r = new Random();
+	int ran = r.nextInt(100);
+	robot[is].move(ran);
+	if(robot[is].getY() <= 20) {
+		robot[is].setY(20);
+		robot[is].turn(90);
+	}
+	if(robot[is].getX() <= 500) {
+		//set X and turn 90 degrees
+	}
+	if(robot[is].getY() == 0 ) {
+		if(robot[is].getX() == 0) {
+			raceOver = true;
+			winnerSaver = is + 1;
+		break;
+		}
+	}
+	
+}
+
+}
+JOptionPane.showMessageDialog(null, "Robot " + winnerSaver + " is the winner!");
+JOptionPane.showMessageDialog(null, "The party was legendary! It was remembered as 'The Great Robot Race' for all time.");
 	//2. create an array of 5 robots.
 
 	//3. use a for loop to initialize the robots.
@@ -19,4 +60,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
